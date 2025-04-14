de93191236eb70b2cf802075fbe03bc99ca2ceec
juziss <julianeta26@gmail.com>
2025-04-14 16:14:25 -0300

    feat(navbar): update design

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
 
   const navItems = [
     { name: "Sobre Mim", to: "#sobre" },  // Alteração de href para to
-    { name: "Habilidades & Ferramentas", to: "#habilidades" },  // Alteração de href para to
+    { name: "Skills", to: "#habilidades" },  // Alteração de href para to
     { name: "Experiência", to: "#experiencia" },  // Alteração de href para to
     { name: "Serviços", to: "#servicos" },  // Alteração de href para to
     { name: "Projetos", to: "#projetos" },  // Alteração de href para to
@@ -32,14 +32,14 @@ export default function Navbar() {
   return (
     <div className="fixed left-0 top-0 z-50 w-full px-4 py-4">
       <nav
-        className={`mx-auto flex max-w-6xl items-center justify-between rounded-full px-6 py-3 backdrop-blur-md transition-all duration-300 ${
+        className={`mx-auto flex max-w-6xl items-center justify-between rounded-full px-6 py-3 backdrop-blur-md transition-all duration-300 border border-white/30 ${
           scrolled ? "border border-white/10 bg-white/10 shadow-lg" : "border border-transparent bg-white/5"
         }`}
       >
         {/* Logo */}
         <div className="flex items-center">
           <Link to="/" className="text-xl font-bold">
-            Portfolio
+            <img src="\public\favicon.ico" alt="Logo" className="size-10" />
           </Link>
         </div>
 
@@ -49,7 +49,9 @@ export default function Navbar() {
             <Link
               key={item.name}
               to={item.to}  // Alteração de href para to
-              className="rounded-full px-4 py-2 text-sm font-medium transition-colors hover:bg-white/10"
+              className="rounded-full px-6 py-2 text-sm text-gray-500 hover:text-pink-500 font-medium transition-colors hover:bg-white-100/50 border border-transparent hover:border-white/30"
+
+
             >
               {item.name}
             </Link>
