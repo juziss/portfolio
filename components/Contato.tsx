ebb9c71e4273b554764a9083553bb27ebf4477cb
juziss <julianeta26@gmail.com>
2025-04-29 15:46:42 -0300

    feature(contato): create section and elements

--- components/Contato.tsx
+++ components/Contato.tsx
@@ -35,7 +35,7 @@ export default function Contato() {
   };
 
   return (
-    <section id="contato" className="relative py-20 px-8 bg-gray-50">
+    <section id="contato" className="relative py-20 px-8">
       {/* Título da seção */}
       <h2 className="text-5xl font-extrabold text-gray-800 mb-16 text-center">
         Contato
@@ -128,11 +128,11 @@ export default function Contato() {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
-                juliacoutinho@exemplo.com
+                julianeta26@gmail.com
               </a>
               
               <a 
-                href="https://wa.me/5500000000000" 
+                href="https://wa.me/5538998459457" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="flex items-center text-gray-700 hover:text-pink-500 transition duration-300"
@@ -151,7 +151,7 @@ export default function Contato() {
             <div className="flex space-x-4">
               {/* LinkedIn */}
               <a 
-                href="https://linkedin.com/" 
+                href="https://linkedin.com/in/jucoutinho" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="bg-gray-100 hover:bg-pink-100 p-3 rounded-full transition duration-300"
@@ -160,34 +160,10 @@ export default function Contato() {
                   <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
                 </svg>
               </a>
-              
-              {/* Instagram */}
-              <a 
-                href="https://instagram.com/" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="bg-gray-100 hover:bg-pink-100 p-3 rounded-full transition duration-300"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#d1416d" viewBox="0 0 24 24">
-                  <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
-                </svg>
-              </a>
-              
-              {/* Behance */}
-              <a 
-                href="https://behance.net/" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="bg-gray-100 hover:bg-pink-100 p-3 rounded-full transition duration-300"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#d1416d" viewBox="0 0 24 24">
-                  <path d="M22 7h-7v-2h7v2zm1.726 10c-.442 1.297-2.029 3-5.101 3-3.074 0-5.564-1.729-5.564-5.675 0-3.91 2.325-5.92 5.466-5.92 3.082 0 4.964 1.782 5.375 4.426.078.506.109 1.188.095 2.14h-8.027c.13 3.211 3.483 3.312 4.588 2.029h3.168zm-7.686-4h4.965c-.105-1.547-1.136-2.219-2.477-2.219-1.466 0-2.277.768-2.488 2.219zm-9.574 6.988h-6.466v-14.967h6.953c5.476.081 5.58 5.444 2.72 6.906 3.461 1.26 3.577 8.061-3.207 8.061zm-3.466-8.988h3.584c2.508 0 2.906-3-.312-3h-3.272v3zm3.391 3h-3.391v3.016h3.341c3.055 0 2.868-3.016.05-3.016z"/>
-                </svg>
-              </a>
-              
+
               {/* GitHub */}
               <a 
-                href="https://github.com/" 
+                href="https://github.com/juziss" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="bg-gray-100 hover:bg-pink-100 p-3 rounded-full transition duration-300"
